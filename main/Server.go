30616031b7dd97de064dbfeb9d5f93b731101a41
chenwei <798077078@qq.com>
2019-11-12 10:49:45 +0800

    v0.6

---
+++ main/Server.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"zinx/ziface"
+	"zinx/znet"
+)
+
+//ping test 自定义路由
+type PingRouter struct {
+	znet.BaseRouter     //一定要先基础BaseRouter
+}
+
+//Ping Handle
+func (this *PingRouter) Handle(request ziface.IRequest) {
+	fmt.Println("Call PingRouter Handle")
+	//先读取客户端的数据，再回写ping...ping...ping
+	fmt.Println("recv from client : msgId=", request.GetMsgID(),
+		", data=", string(request.GetData()))
+	err := request.GetConnection().SendMsg(0, []byte("ping...ping...ping"))
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+//HelloZinxRouter Handle
+type HelloZinxRouter struct {
+	znet.BaseRouter
+}
+
+func (this *HelloZinxRouter) Handle(request ziface.IRequest) {
+	fmt.Println("Call HelloZinxRouter Handle")
+	//先读取客户端的数据，再回写ping...ping...ping
+	fmt.Println("recv from client : msgId=", request.GetMsgID(),
+		", data=", string(request.GetData()))
+	err := request.GetConnection().SendMsg(1, []byte("Hello ZinxRouter V0.6"))
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+
+/*
+服务端
+ */
+func main() {
+
+	//1 创建一个server 句柄 s
+	s := znet.NewServer()
+
+	s.AddRouter(0,&PingRouter{})
+	s.AddRouter(1, &HelloZinxRouter{})
+
+	//2.开启服务
+	s.Server()
+
+}
